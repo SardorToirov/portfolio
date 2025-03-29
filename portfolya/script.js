f846231029759f21cdb49e4d0ad5d1378b9c30be
Sardor Toirov <102356919+SardorToirov@users.noreply.github.com>
2025-03-29 21:03:22 +0500

    Add files via upload

---
+++ portfolya/script.js
@@ -0,0 +1,7 @@
+// ===================typing animation=======================
+var typed = new Typed(".typing",{
+    strings:[" ","Backend Developer", "wep Developer", "software engineer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
